ad4ebc17bb4c856b2a2c7294f5e947be7c1df71c
SmileBeanDev <smilebeandev@gmail.com>
2025-06-05 16:06:46 +0900

    Initial commit

---
+++ src/main/java/dev/smilebean/ysb_hw1/YsbHw1Application.java
@@ -0,0 +1,13 @@
+package dev.smilebean.ysb_hw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsbHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YsbHw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/smilebean/ysb_hw1/YsbHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.smilebean.ysb_hw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsbHw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
